7d7f76bf2751bb5ec19d9aec62360472d9d22ea6
Liam Wilson <cosinusoidally@gmail.com>
2022-02-05 20:10:32 +0000

    more examples

--- tests/c_snippets/trivial_c.js
+++ tests/c_snippets/trivial_c.js
@@ -30,7 +30,21 @@ int f1(void){\n\
   return 2+x;\n\
 }";
 print(c_src);
-
+print();
 obj_code=mm.load_c_string(c_src);
 linked=mm.link([obj_code]);
 print("f1(): "+linked.run("f1")());
+print("lets have f2 calling f1");
+c_src="\
+int x=100;\n\
+int f1(int y){\n\
+  return y+x;\n\
+}\n\
+int f2(void){\n\
+return f1(5);\n\
+}\n";
+print(c_src);
+
+obj_code=mm.load_c_string(c_src);
+linked=mm.link([obj_code]);
+print("f2(): "+linked.run("f2")());
