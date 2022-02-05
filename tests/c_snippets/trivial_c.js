b9812003512b47705a087044f598a206bb2eb6c7
Liam Wilson <cosinusoidally@gmail.com>
2022-02-05 14:57:41 +0000

    add dummy code example

--- tests/c_snippets/trivial_c.js
+++ tests/c_snippets/trivial_c.js
@@ -1,1 +1,13 @@
 print("in trivial_c.js");
+
+c_src="\
+int f1(void){\n\
+  return 7;\n\
+}";
+
+print("Aim of the game is to load and run the following snippet of C");
+print(c_src);
+
+obj_code=mm.load_c_string(c_src);
+
+print(obj_code.run("f1")());
