16fb9065a03547f56603c4b72c5b13a2b98912f7
Liam Wilson <cosinusoidally@gmail.com>
2022-02-05 19:32:18 +0000

    add some more simple c examples

--- tests/c_snippets/trivial_c.js
+++ tests/c_snippets/trivial_c.js
@@ -11,3 +11,26 @@ print(c_src);
 obj_code=mm.load_c_string(c_src);
 linked=mm.link([obj_code]);
 print("f1(): "+linked.run("f1")());
+print();
+print("ok, and now another");
+c_src="\
+int f1(void){\n\
+  return 2;\n\
+}";
+print(c_src);
+
+obj_code=mm.load_c_string(c_src);
+linked=mm.link([obj_code]);
+print("f1(): "+linked.run("f1")());
+print();
+print("A more complex example");
+c_src="\
+int x=100;\n\
+int f1(void){\n\
+  return 2+x;\n\
+}";
+print(c_src);
+
+obj_code=mm.load_c_string(c_src);
+linked=mm.link([obj_code]);
+print("f1(): "+linked.run("f1")());
