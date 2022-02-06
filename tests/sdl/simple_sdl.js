570814fff5c7411f9a3481bf421d57739820fd5d
Liam Wilson <cosinusoidally@gmail.com>
2022-02-06 14:29:58 +0000

    load some sdl symbols so simpld_sdl.js starts

--- tests/sdl/simple_sdl.js
+++ tests/sdl/simple_sdl.js
@@ -1,7 +1,7 @@
 print("This is a simple SDL example");
 load("lib/setup_sdl.js");
 obj_code=mm.load_c_string(read(test_path+"/simple_sdl.c"));
-lib=mm.link([obj_code]);
+lib=mm.link([obj_code,libsdl.syms]);
 
 
 lib.run("init_sdl")();
