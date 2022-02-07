f5148c6fabf4ececbeee3c99092d819230d23c84
Liam Wilson <cosinusoidally@gmail.com>
2022-02-07 19:28:21 +0000

    stub out quake1 test

---
+++ tests/quake1/quake.js
@@ -0,0 +1,5 @@
+print("Run the quake1 engine");
+print("you need the engine source in ../quake1_src and id1 pak files in ../quake1_data/id1 (relative to the root of mishmashvm)");
+
+
+print("unfinished might not work yet");
