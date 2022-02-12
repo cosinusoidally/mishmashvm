fde32c297e4b38c547d7df9389a83aa8a5b29c1a
Liam Wilson <cosinusoidally@gmail.com>
2022-02-12 19:58:26 +0000

    duktape dummy wrappers

--- tests/duktape/duk.js
+++ tests/duktape/duk.js
@@ -1,9 +1,12 @@
 print("Duktape");
+load("lib/gen_wrap.js");
 
 duk_srcdir=test_path+"/duktape_src/";
 
 duk_glue=mm.load_c_string(read(test_path+"/duk_glue.c"),{extra_flags:"-I "+duk_srcdir});
 duktape=mm.load_c_string(read(duk_srcdir+"duktape.c"),{extra_flags:"-I "+duk_srcdir});
+
+libtcc1=mm.load_c_string(read("tcc_src/lib/libtcc1.c"));
 
 dump_und=true;
 
@@ -30,13 +33,21 @@ if(dump_und=true){
   und=und.sort();
   var stubs_src=[];
   stubs_src.push("ljw_stubs(){");
+  my_libc_src=[];
   for(var i=0;i<und.length;i++){
-    stubs_src.push(und[i]+"();");
+    var s=und[i];
+    stubs_src.push(s+"();");
+    my_libc_src.push("ljw_crash_"+s+"(){printf(\"unimplemented: "+s+"\\n\");exit(1);}");
   };
+  my_libc_src= my_libc_src.join("\n");
   stubs_src.push("}");
   stubs_src=stubs_src.join("\n");
   print("stubs:");
   print(stubs_src);
   stubs=mm.load_c_string(stubs_src);
   print(JSON.stringify(overrides, null, " "));
+  print(my_libc_src);
+  my_libc=mm.load_c_string(my_libc_src);
 };
+
+my_wrap=mm.gen_wrap(my_libc,stubs,overrides);
