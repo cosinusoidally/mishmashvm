78be4e37624c72aad93d77e40149a7e9c9d3ceca
Liam Wilson <cosinusoidally@gmail.com>
2022-02-12 18:31:13 +0000

    print out undefined duktape symbols

--- tests/duktape/duk.js
+++ tests/duktape/duk.js
@@ -5,3 +5,14 @@ duk_srcdir=test_path+"/duktape_src/";
 duk_glue=mm.load_c_string(read(test_path+"/duk_glue.c"),{extra_flags:"-I "+duk_srcdir});
 duktape=mm.load_c_string(read(duk_srcdir+"duktape.c"),{extra_flags:"-I "+duk_srcdir});
 
+dump_und=true;
+
+if(dump_und=true){
+  und=[];
+  for(var i=0;i<duktape.und.length;i++){
+    print(und.push(duktape.und[i].st_name));
+  };
+  und.push("printf");
+  und.push("exit");
+  print(JSON.stringify(und));
+}
