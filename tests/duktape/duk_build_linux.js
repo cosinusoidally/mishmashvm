43fe67fe97a13a228d1bbe70fce56093339c05de
Liam Wilson <cosinusoidally@gmail.com>
2022-02-17 18:24:33 +0000

    closer to native Linux Duktape

--- tests/duktape/duk_build_linux.js
+++ tests/duktape/duk_build_linux.js
@@ -1,1 +1,1 @@
-mm.compile("tcc tests/duktape/duk_main_linux.c tests/duktape/duk_glue.c tests/duktape/duktape_src/duktape.c -I tests/duktape/duktape_src/ -o duk_linux.exe -lm",{no_std_flags:true});
+mm.compile("tcc tests/duktape/duk_main_linux.c tests/duktape/duk_glue.c tests/duktape/duktape_src/duktape.c -I tests/duktape/duktape_src/ -o duk_linux.exe -lm -ldl",{no_std_flags:true});
--- tests/duktape/duk_main_linux.c
+++ tests/duktape/duk_main_linux.c
@@ -1,4 +1,45 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <dlfcn.h>
+
+void* L[8];
+uint32_t H=0;
+
+int ctypes_open(char *s){
+  printf("Opening lib: %s\n",s);
+  void *p=dlopen(s,RTLD_LAZY);
+  printf("p=%u\n",p);
+  if(p!=NULL){
+    L[H]=p;
+    H++;
+    return H;
+  };
+  return 0;
+}
+
+int ctypes_getsym(uint32_t h,char *s){
+  printf("Looking up: %s in %u\n",s,h);
+  printf("handle %u\n",L[h]);
+  return 1;
+}
+
 main(){
-init();
-my_duk_run("print('hello world from Linux duktape')");
+  init();
+  FILE *f;
+  f=fopen("tests/duktape/duk_mishmashvm_support.js","rb");
+  fseek(f,0,SEEK_END);
+  uint32_t l=ftell(f);
+  rewind(f);
+  char *b;
+  b=malloc(l);
+  fread(b,1,l,f);
+  fclose(f);
+  char buf[128];
+  sprintf(buf, "print(fn_ptr2=%u);\n",&puts);
+  my_duk_run(buf);
+  sprintf(buf, "print(ctypes_open_ptr=%u);\n",&ctypes_open);
+  my_duk_run(buf);
+  sprintf(buf, "print(ctypes_getsym_ptr=%u);\n",&ctypes_getsym);
+  my_duk_run(buf);
+  my_duk_run(b);
 }
