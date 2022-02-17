a5320656152bb884e729c55a4655fe6a763958f6
Liam Wilson <cosinusoidally@gmail.com>
2022-02-17 20:29:42 +0000

    more

--- mishmashvm.js
+++ mishmashvm.js
@@ -13,6 +13,7 @@ tests=[
   {"path":"tests/duktape","script":"duk_boot1.js","description":"Test first stage bootstrap with duktape (building native tcc object code using the Emscripten compiled JS version of tcc). Note this test is very slow. Note does not write any files to disk so will not break the existing native tcc code."},
   {"path":"tests/duktape","script":"duk_mishmashvm.js","description":"Run mishmashvm.js inside Duktape."},
   {"path":"tests/duktape","script":"duk_build_linux.js","description":"Build Duktape for Linux"},
+  {"path":"tests/duktape","script":"duk_build_win32.js","description":"Build Duktape for win32"},
 ];
 
 print_tests=function(){
---
+++ tests/duktape/duk_build_win32.js
@@ -0,0 +1,2 @@
+print("Tricky atm just run:");
+print("i386-win32-tcc tests/duktape/duk_main_win32.c  tests/duktape/duk_glue.c tests/duktape/duktape_src/duktape.c -I tests/duktape/duktape_src/ -o duk_win32.exe");
