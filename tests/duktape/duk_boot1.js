935040d2211ed50ce78ac6eb32b6775341696e78
Liam Wilson <cosinusoidally@gmail.com>
2022-02-19 19:47:24 +0000

    update stubs.o hash

--- tests/duktape/duk_boot1.js
+++ tests/duktape/duk_boot1.js
@@ -9,7 +9,7 @@ print("from tcc_js_bootstrap. Note no files are written to disk, this test just 
 print("You might want to get a cup of tea as the build process may take several minutes. Duktape appears to be significantly slower than even the Spidermonkey interpreter (though the slowness could partially be to do with Duktape being compiled with tcc, and/or some of the config flags I had to use).");
 //duk_run("function read(x){print(x)}");
 [
-["03_mk_libc_stubs.js","b41ba780a63def95319727874d39d905bac70630fd0c21e2b8d1bb670c82a71f"],
+["03_mk_libc_stubs.js","0cdb519efb42f22eba96bf90407e10b87868355e1d793d3a70271f09d47aa403"],
 ["02_mk_libc_wrap.js","0e8e02c852a11ebec585650d3c6fa5917fb51d7b66dd1d458d3e51680c660964"],
 ["01_mk_tcc.js","080182b567eabf208f60fcba6182d9c7a11c5f2421398dec530bc9dded852004"]
 ].map(function(x){
