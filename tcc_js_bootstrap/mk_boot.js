7591b9641ffc94f085f66810c79d820e53b6b983
Liam Wilson <cosinusoidally@gmail.com>
2022-02-21 18:13:37 +0000

    Update tcc_js_bootstrap docs

--- tcc_js_bootstrap/mk_boot.js
+++
@@ -1,6 +0,0 @@
-args="-c tcc_src/tcc.c -DCONFIG_TRIPLET=\"i386-linux-gnu\" -DTCC_TARGET_I386 -DONE_SOURCE=1 -Wall -O0 -g -I tcc_src/ -o out.o"
-args=args.split(" ");
-print(JSON.stringify(args));
-load("tcc_em.js");
-Module.arguments=args;
-compile("out.o");
