f79911a3ff87753e29e61b8310f327004be30761
Liam Wilson <cosinusoidally@gmail.com>
2022-02-25 18:44:45 +0000

    more

--- tests/duktape/duk_build_linux.js
+++ tests/duktape/duk_build_linux.js
@@ -19,6 +19,7 @@ print();
 print("Building ....");
 print("Once complete run chmod +x duk_linux.exe and then ./duk_linux.exe");
 print("If it fails look in stderr.txt or stdout.txt for clues");
+mm.writeFile(mm.cfg.tmpdir+"/duk_boot_code.h","char *duk_boot_code="+JSON.stringify(read(test_path+"/duk_mishmashvm_support.js"))+";");
 r=mm.compile("tcc -g -nostdinc -nostdlib -I ./includes/usr/include/:./includes/usr/include/i386-linux-gnu/:./includes/tmp/tcc/lib/tcc/include/:"+mm.cfg.tmpdir+" ../linux_lib_bin/crt1.o ../linux_lib_bin/crti.o ../linux_lib_bin/crtn.o tests/duktape/duk_main_linux.c tests/duktape/duk_glue.c tests/duktape/duktape_src/duktape.c libc_portable_proto/tcc_bin/libtcc1.o ../linux_lib_bin/libc_nonshared.a -I tests/duktape/:tests/duktape/duktape_src/ -o duk_linux.exe -L../linux_lib_bin/ -L/lib32/ -L /lib/i386-linux-gnu/ -lm -ldl",{no_std_flags:true});
 if(r!==0){
   print("Build failed. Note the Linux binary can only be built on Linux");
