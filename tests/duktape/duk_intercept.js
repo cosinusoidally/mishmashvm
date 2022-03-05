946542af0e1d4786b672a30d1405410862fe0903
Liam Wilson <cosinusoidally@gmail.com>
2022-03-05 16:22:33 +0000

    tidy up

--- tests/duktape/duk_intercept.js
+++ tests/duktape/duk_intercept.js
@@ -59,41 +59,39 @@ exclude={
 }
 overrides=[];
 
-if(dump_und=true){
-  und=[];
-  for(var i=0;i<duktape.und.length;i++){
-    var c=duktape.und[i].st_name;
-    und.push(c);
-    if(!exclude[c]){
-      if(!passthrough[c]){
-        d="ljw_crash_"+c;
-      } else {
-        d=c;
-      };
-      overrides.push([d,c]);
+und=[];
+for(var i=0;i<duktape.und.length;i++){
+  var c=duktape.und[i].st_name;
+  und.push(c);
+  if(!exclude[c]){
+    if(!passthrough[c]){
+      d="ljw_crash_"+c;
+    } else {
+      d=c;
     };
+    overrides.push([d,c]);
   };
-  und=und.sort();
-  var stubs_src=[];
-  stubs_src.push("ljw_stubs(){");
-  my_libc_src=[];
-  for(var i=0;i<und.length;i++){
-    var s=und[i];
-    if(!exclude[s]){
-      stubs_src.push(s+"();");
-      my_libc_src.push("ljw_crash_"+s+"(){printf(\"unimplemented: "+s+"\\n\");exit(1);}");
-    };
+};
+und=und.sort();
+var stubs_src=[];
+stubs_src.push("ljw_stubs(){");
+my_libc_src=[];
+for(var i=0;i<und.length;i++){
+  var s=und[i];
+  if(!exclude[s]){
+    stubs_src.push(s+"();");
+    my_libc_src.push("ljw_crash_"+s+"(){printf(\"unimplemented: "+s+"\\n\");exit(1);}");
   };
-  my_libc_src= my_libc_src.join("\n");
-  stubs_src.push("}");
-  stubs_src=stubs_src.join("\n");
+};
+my_libc_src= my_libc_src.join("\n");
+stubs_src.push("}");
+stubs_src=stubs_src.join("\n");
 //  print("stubs:");
 //  print(stubs_src);
-  stubs=mm.load_c_string(stubs_src);
+stubs=mm.load_c_string(stubs_src);
 //  print(JSON.stringify(overrides, null, " "));
 //  print(my_libc_src);
-  my_libc=mm.load_c_string(my_libc_src);
-};
+my_libc=mm.load_c_string(my_libc_src);
 
 my_wrap=mm.gen_wrap(my_libc,stubs,overrides);
 
