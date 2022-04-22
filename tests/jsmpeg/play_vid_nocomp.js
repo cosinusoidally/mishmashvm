9fec2dab045f84f01430a0e79d72e880a1d9624b
Liam Wilson <cosinusoidally@gmail.com>
2022-04-22 20:18:31 +0000

    clean up jsmpeg demo

--- tests/jsmpeg/play_vid_nocomp.js
+++
@@ -1,3 +0,0 @@
-load("../lib/setup_platform.js"); 
-libc.chdir("jsmpeg_demo");
-run("test_win32_sdl.js");
--- tests/jsmpeg/play_vid_nocomp_linux.js
+++
@@ -1,3 +0,0 @@
-load("../lib/setup_platform.js"); 
-libc.chdir("jsmpeg_demo");
-run("test_sdl2.js");
