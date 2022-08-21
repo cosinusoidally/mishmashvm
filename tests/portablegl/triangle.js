038cfcbdff69670280340738443922b8548868e6
Liam Wilson <cosinusoidally@gmail.com>
2022-08-21 19:00:44 +0000

    update triangle.js

---
+++ tests/portablegl/triangle.js
@@ -0,0 +1,54 @@
+print("GL triangle");
+
+load("lib/gen_wrap.js");
+
+
+libtcc1=mm.load_c_string(read("tcc_src/lib/libtcc1.c"));
+
+obj=mm.load_c_string(read("sdl1_main.c"),{extra_flags:"-I triangle"});
+
+passthrough={
+};
+
+exclude={
+};
+
+overrides=[];
+
+und=[];
+for(var i=0;i<obj.und.length;i++){
+  var c=obj.und[i].st_name;
+  und.push(c);
+  if(!exclude[c]){
+    if(!passthrough[c]){
+      d="ljw_crash_"+c;
+    } else {
+      d=c;
+    };
+    overrides.push([d,c]);
+  };
+};
+und=und.sort();
+var stubs_src=[];
+stubs_src.push("ljw_stubs(){");
+my_libc_src=[];
+for(var i=0;i<und.length;i++){
+  var s=und[i];
+  if(!exclude[s]){
+    stubs_src.push(s+"();");
+    my_libc_src.push("ljw_crash_"+s+"(){printf(\"unimplemented: "+s+"\\n\");exit(1);}");
+  };
+};
+my_libc_src= my_libc_src.join("\n");
+stubs_src.push("}");
+stubs_src=stubs_src.join("\n");
+//  print("stubs:");
+//  print(stubs_src);
+stubs=mm.load_c_string(stubs_src);
+//  print(JSON.stringify(overrides, null, " "));
+//  print(my_libc_src);
+my_libc=mm.load_c_string(my_libc_src);
+
+my_wrap=mm.gen_wrap(my_libc,stubs,overrides);
+
+demo=mm.link([obj,libsdl.syms,my_wrap,libtcc1]);
