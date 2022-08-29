f9a01acb9e9355dc0cf1d199ac4c2dd6f073ee0d
Liam Wilson <cosinusoidally@gmail.com>
2022-08-29 14:47:41 +0000

    pp more compat

--- tests/portablegl/penguin.js
+++ tests/portablegl/penguin.js
@@ -4,4 +4,22 @@ name="penguin";
 
 load(test_path+"/main.js");
 
+// compat code:
+window={};
+document={};
+document.getElementById=function(n){
+  if(n==="mycanvas"){
+    return {};
+  };
+  throw "unsupported element name";
+};
+function alert(x){
+print("ALERT: "+x);
+};
+// load demo
+load(test_path+"/penguin/penguin.js");
+
+//run demo
+webGLStart();
+
 //demo.get_fn("sdl_setup_context")();
