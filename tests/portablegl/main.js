d6cce947b1a30c33ef7eaf3a17a0f50c90672eeb
Liam Wilson <cosinusoidally@gmail.com>
2022-09-09 16:19:20 +0000

    pp allow option to load shared library version of pgl for benchmarking

--- tests/portablegl/main.js
+++ tests/portablegl/main.js
@@ -5,6 +5,13 @@ libtcc1=mm.load_c_string(read("tcc_src/lib/libtcc1.c"));
 
 obj=mm.load_c_string(read(test_path+"/sdl1_main.c"),{extra_flags:"-I"+test_path+" -I "+test_path+"/"+name});
 
+// this allows benchmarking against a shared library
+// version of pgl
+var use_so;
+if(use_so){
+  load_pgl_so();
+};
+
 passthrough={
   "malloc": true,
   "memmove": true,
@@ -68,3 +75,53 @@ my_libc=mm.load_c_string(my_libc_src);
 my_wrap=mm.gen_wrap(my_libc,stubs,overrides);
 
 demo=mm.link([obj,libsdl.syms,my_wrap,libtcc1]);
+
+function load_pgl_so(){
+  print("use so");
+// must be a global to prevent gc
+  so=ctypes.open("pgl.so");
+  var s=[
+  "update",
+  "glFrontFace",
+  "glClearColor",
+  "glClear",
+  "glCullFace",
+  "glEnable",
+  "glDepthRange",
+  "glClearDepth",
+  "glGenTextures",
+  "glActiveTexture",
+  "glBindTexture",
+  "glTexImage2D",
+  "glTexParameteri",
+  "create_program",
+  "pglSetUniform",
+  "glGenBuffers",
+  "glBindBuffer",
+  "glBufferData",
+  "glVertexAttribPointer",
+  "glEnableVertexAttribArray",
+  "glDepthMask",
+  "glDrawArrays",
+  "glDrawElements",
+  "glUseProgram",
+  "set_size",
+  "set_scale",
+  "setup_context",
+  "sdl_setup_context",
+  "show_consts",
+  "get_shader_unform_metadata",
+  "get_shader_attributes_metadata",
+  "get_event_info",
+  "shader_vs",
+  "shader_fs",
+  "wrap_glClearColor",
+  "wrap_glDepthRange",
+  "wrap_glClearDepth",
+  "wrap_glBindTexture",
+  "wrap_glTexImage2D"
+  ];
+  for(var i=0;i<s.length;i++){
+    obj.sections[".symtab"].byname[s[i]].address= ctypes.cast(so.declare(s[i],ctypes.default_abi,ctypes.uint32_t), ctypes.uint32_t).value;
+  };
+};
