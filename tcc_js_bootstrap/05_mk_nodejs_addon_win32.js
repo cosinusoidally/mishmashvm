a5cfd68d9b6af1d368972ef5c782ac9d298af96e
Liam Wilson <cosinusoidally@gmail.com>
2022-10-05 18:49:59 +0000

    start trying to build win32 nodejs addon during bootstrap

---
+++ tcc_js_bootstrap/05_mk_nodejs_addon_win32.js
@@ -0,0 +1,21 @@
+load("tcc_em.js");
+FS.mkdir("include");
+FS.mkdir("include/node");
+f={
+  "bootstrap.c":"../tests/nodejs/bootstrap.c",
+  "binding.c":"../tests/nodejs/binding.c",
+  "binding_linux.c":"../tests/nodejs/binding_linux.c",
+  "common.h":"../tests/nodejs/common.h",
+  "binding.h":"../tests/nodejs/binding.h",
+  "include/node/node_api.h":"../tests/nodejs/include/node/node_api.h",
+  "include/node/node_api_types.h":"../tests/nodejs/include/node/node_api_types.h"
+}
+for(i in f){
+  FS.writeFile(i, read(f[i]));
+};
+//args="-c bootstrap.c -I . -I include/node/ -o out.o"
+args="-nostdinc -nostdlib -I /usr/include/:/usr/include/i386-linux-gnu/:/tmp/tcc/lib/tcc/include/ -c bootstrap.c -I . -I include/node/ -o out.o"
+args=args.split(" ");
+print(JSON.stringify(args));
+Module.arguments=args;
+compile("../tests/nodejs/lib/addon.o");
