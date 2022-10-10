d37a24c5183e1c6b92d9aef81488047beb8e5e88
Liam Wilson <cosinusoidally@gmail.com>
2022-10-10 18:23:13 +0000

    node.js bootstrap

--- bootstrap_nodejs.js
+++ bootstrap_nodejs.js
@@ -1,1 +1,20 @@
-console.log("Bootstrapping using node.js");
+console.log("Bootstrapping using node.js. Requires node.js >= 12.x");
+
+process=require("process");
+child_process=require("child_process");
+os=require("os");
+console.log(process.execPath);
+
+run=function(x){
+  console.log("Running: "+x);
+  var r=child_process.spawnSync(process.execPath,["node_compat_min.js",x]);
+  console.log("stdout:");
+  console.log(r.output[1].toString());
+  console.log("stderr:");
+  console.log(r.output[2].toString());
+};
+
+process.chdir("tcc_js_bootstrap");
+
+run("04_mk_nodejs_addon.js");
+run("05_mk_nodejs_addon_win32.js");
