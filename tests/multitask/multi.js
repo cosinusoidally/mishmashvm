1dec956d2a6393fc09af7bba1adee44ae8df1a7a
Liam Wilson <cosinusoidally@gmail.com>
2022-11-27 18:40:14 +0000

    start adding in PR_CreateProcess

--- tests/multitask/multi.js
+++ tests/multitask/multi.js
@@ -1,1 +1,21 @@
 print("Multitasking demo");
+
+nspr={};
+
+process={};
+
+if(plat==="linux32"){
+  print("loading linux nspr");
+  nspr.lib=ctypes.open("libnspr4.so");
+  process.execPath="js";
+};
+
+if(plat==="win32"){
+  print("loading win32 nspr");
+  nspr.lib=ctypes.open("nss3.dll");
+  process.execPath="js.exe";
+}
+
+nspr.PR_CreateProcess=nspr.lib.declare("PR_CreateProcess",ctypes.default_abi,
+                                   ctypes.uint32_t,
+                                   ctypes.char.ptr,ctypes.voidptr_t,ctypes.uint32_t,ctypes.uint32_t);
