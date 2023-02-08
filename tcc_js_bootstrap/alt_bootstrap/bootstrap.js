8024044aa1017d9fd3e9d6a38f84ccf36585cb9c
Liam Wilson <cosinusoidally@gmail.com>
2023-02-08 13:53:38 +0000

    update path to hex0 and kaem

--- tcc_js_bootstrap/alt_bootstrap/bootstrap.js
+++ tcc_js_bootstrap/alt_bootstrap/bootstrap.js
@@ -17,12 +17,12 @@ hex0_to_array=function(x){
   return b.map(function(x){return parseInt(x,16)});
 };
 
-hex0=hex0_to_array(read("stage0/x86/hex0_x86.hex0"));
+hex0=hex0_to_array(read("stage0-posix/x86/hex0_x86.hex0"));
 hex0_sha256=root.sha256(hex0);
 hex0_sha256_expected="e650a1b5bdcfd79689569a3199a252a76622f7ea4fe036a5dddc2039fdad79c5";
 print("hex0 sha256: "+hex0_sha256+" "+(hex0_sha256===hex0_sha256_expected));
 
-kaem=hex0_to_array(read("stage0/x86/kaem-minimal.hex0"));
+kaem=hex0_to_array(read("stage0-posix/x86/kaem-minimal.hex0"));
 kaem_sha256=root.sha256(kaem);
 kaem_sha256_expected="4fd5d6d7f1ac4708c06df2bf7e0b7f6dc45a493ac100e14fc52172929b807f5e";
 print("kaem sha256: "+kaem_sha256+" "+(kaem_sha256===kaem_sha256_expected));
