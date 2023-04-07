af5948855bea2d94e240ca2bece43a5923596b99
Liam Wilson <cosinusoidally@gmail.com>
2023-04-07 16:59:30 +0000

    more progress on testing M2

--- tcc_js_bootstrap/alt_bootstrap/test_m2.js
+++ tcc_js_bootstrap/alt_bootstrap/test_m2.js
@@ -1,5 +1,23 @@
-path='../../../mmaux/snap21/';
+print("make sure there is already a generated snapshot in path");
+path='../../../mmaux/snap2/';
 use_snap=true;
 load('bootstrap_proto17.js');
 use_snap=false;
 path=undefined;
+
+var go=function(x){
+  vfs.writeFile("/kaem.x86",string_to_arr(x));
+  kernel.run3();
+};
+
+vfs.writeFile("/foo.c",string_to_arr(
+[
+"blah",
+].join("\n")
+));
+script="./x86/artifact/M2  --architecture x86 -f ./foo.c -o ./foo.M1\n";
+go(script);
+print("stderr:");
+print(get_stderr(pt[2]));
+print("stdout:");
+print(get_stdout(pt[2]));
