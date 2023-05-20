e7118a31e951e5dd41b217b5e75596b977739c06
Liam Wilson <cosinusoidally@gmail.com>
2023-05-20 15:05:39 +0100

    more progress towards being about to optionally bootstrap with a stock firefox

--- auxiliary/bootstrap_xpc.sh
+++ auxiliary/bootstrap_xpc.sh
@@ -1,14 +1,20 @@
 echo "Bootstrap"
 
-cd tcc_js_bootstrap
+cmd (){
+  r="exe='$1';load('../lib/xpc.js')"
+  echo $r
+  firefox -xpcshell -e "$r"
+}
+
+cd ../tcc_js_bootstrap
 echo "Building tcc with Emscripten compiled tcc"
-js --no-ion 01_mk_tcc.js
+cmd 01_mk_tcc.js
 
 echo "Building libc wrapper with Emscripten compiled tcc"
-js --no-ion 02_mk_libc_wrap.js
+cmd 02_mk_libc_wrap.js
 
 echo "Building libc unimplemented stubs with Emscripten compiled tcc"
-js --no-ion 03_mk_libc_stubs.js
+cmd 03_mk_libc_stubs.js
 cd ..
 
 cd libc_portable_proto
