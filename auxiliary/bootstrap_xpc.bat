694d4cad7499c88eab25e884ff7445ee8ac7cca8
Liam Wilson <cosinusoidally@gmail.com>
2023-05-20 16:25:06 +0100

    improve win32 xpcshell bootstrap

--- auxiliary/bootstrap_xpc.bat
+++ auxiliary/bootstrap_xpc.bat
@@ -1,16 +1,18 @@
 set js_cmd=../../firefox_win32/firefox.exe -xpcshell
 set start_cmd=start /wait /B
+set f1=-e "exe=
+set f2=;load('../lib/xpc.js')"
 echo "Bootstrap"
 
 cd ../tcc_js_bootstrap
 echo "Building tcc with Emscripten compiled tcc"
-%start_cmd% %js_cmd% -e "exe='01_mk_tcc.js';load('../lib/xpc.js')"
+%start_cmd% %js_cmd% %f1%'01_mk_tcc.js'%f2%
 
 echo "Building libc wrapper with Emscripten compiled tcc"
-%start_cmd% %js_cmd% 02_mk_libc_wrap.js
+%start_cmd% %js_cmd% %f1%'02_mk_libc_wrap.js'%f2%
 
 echo "Building libc unimplemented stubs with Emscripten compiled tcc"
-%start_cmd% %js_cmd% 03_mk_libc_stubs.js
+%start_cmd% %js_cmd% %f1%'03_mk_libc_stubs.js'%f2%
 cd ..
 
 cd libc_portable_proto
