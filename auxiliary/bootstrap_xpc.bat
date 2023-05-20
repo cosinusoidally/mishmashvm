381a5cbce0a2b3fe8f77dc359bb6a137184ad01c
Liam Wilson <cosinusoidally@gmail.com>
2023-05-20 16:35:05 +0100

    completed alternative bootstrap path using firefox on win32

--- auxiliary/bootstrap_xpc.bat
+++ auxiliary/bootstrap_xpc.bat
@@ -18,3 +18,66 @@ cd ..
 cd libc_portable_proto
 
 REM bootstrap_tcc.bat
+
+echo "2nd stage bootstrap"
+
+echo "Building libtcc1.o"
+%start_cmd% %js_cmd% %f1%'01.js'%f2%
+
+echo "Starting 3 stage native bootstrap"
+
+echo "Building tcc (stage 1)"
+%start_cmd% %js_cmd% %f1%'02.js'%f2%
+
+echo "Building libtcc1.o (stage 1)"
+%start_cmd% %js_cmd% %f1%'03.js'%f2%
+
+del tcc_bin\libtcc1.o
+del tcc_bin\tcc_boot3.o
+
+move tcc_bin\libtcc1.o.new tcc_bin\libtcc1.o
+move tcc_bin\tcc_boot3.o.new tcc_bin\tcc_boot3.o
+
+echo "Building tcc (stage 2)"
+%start_cmd% %js_cmd% %f1%'02.js'%f2%
+
+echo "Building libtcc1.o (stage 2)"
+%start_cmd% %js_cmd% %f1%'03.js'%f2%
+
+del tcc_bin\libtcc1.o
+del tcc_bin\tcc_boot3.o
+
+move tcc_bin\libtcc1.o.new tcc_bin\libtcc1.o
+move tcc_bin\tcc_boot3.o.new tcc_bin\tcc_boot3.o
+
+echo "Building tcc (stage 3)"
+%start_cmd% %js_cmd% %f1%'02.js'%f2%
+
+echo "Building libtcc1.o (stage 3)"
+%start_cmd% %js_cmd% %f1%'03.js'%f2%
+
+echo "building libc wrapper/stubs (stage 1)"
+
+REM call "bootstrap_libc.bat"
+echo "Building my_libc.o.new"
+%start_cmd% %js_cmd% %f1%'mk_libc.js'%f2%
+
+echo "Building stubs.o.new"
+%start_cmd% %js_cmd% %f1%'mk_stubs.js'%f2%
+
+
+del my_libc.o
+del stubs.o
+move my_libc.o.new my_libc.o
+move stubs.o.new stubs.o
+
+echo "building libc wrapper/stubs (stage 2)"
+REM call "bootstrap_libc.bat"
+
+echo "Building my_libc.o.new"
+%start_cmd% %js_cmd% %f1%'mk_libc.js'%f2%
+
+echo "Building stubs.o.new"
+%start_cmd% %js_cmd% %f1%'mk_stubs.js'%f2%
+
+%start_cmd% %js_cmd% %f1%'04_check_sha256.js'%f2%
