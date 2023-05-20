beee98e0a0257cec5e97ef3d833a41e20ee632a1
Liam Wilson <cosinusoidally@gmail.com>
2023-05-20 14:48:52 +0100

    more progress

---
+++ auxiliary/bootstrap_xpc.sh
@@ -0,0 +1,16 @@
+echo "Bootstrap"
+
+cd tcc_js_bootstrap
+echo "Building tcc with Emscripten compiled tcc"
+js --no-ion 01_mk_tcc.js
+
+echo "Building libc wrapper with Emscripten compiled tcc"
+js --no-ion 02_mk_libc_wrap.js
+
+echo "Building libc unimplemented stubs with Emscripten compiled tcc"
+js --no-ion 03_mk_libc_stubs.js
+cd ..
+
+cd libc_portable_proto
+
+./bootstrap_tcc.sh
