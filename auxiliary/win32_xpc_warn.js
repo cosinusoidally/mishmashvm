70fa1af197fbc61c94de320375cc8fe2da306948
Liam Wilson <cosinusoidally@gmail.com>
2023-05-20 18:12:26 +0100

    make message shorter

--- auxiliary/win32_xpc_warn.js
+++ auxiliary/win32_xpc_warn.js
@@ -1,4 +1,3 @@
 load("../lib/xpc.js");
 msgbox("Note on win32 firefox in xpcshell mode will not\n\
-write to standard out/error. This can make it hard to\n\
-debug bootstrapping failures");
+write to standard out/error. Hard to debug failures.");
