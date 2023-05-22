fd20ed126f1b17c417937b0ef472e08015a1db11
Liam Wilson <cosinusoidally@gmail.com>
2023-05-22 18:05:26 +0000

    add optional tcc based bootstrap path

---
+++ auxiliary/bootstrap_with_tcc.sh
@@ -0,0 +1,31 @@
+INCS="-I ../includes/tmp/tcc/lib/tcc/include/ -I ../includes/usr/include/i386-linux-gnu/  -I ../includes/usr/include/"
+
+cd ../tcc_js_bootstrap
+echo "build tcc_linux.exe"
+rm tcc_linux.exe
+set -e
+tcc_bootstrap.exe ../../tcc_bootstrap/tcc_bootstrap.c -ldl -o tcc_linux.exe
+chmod +x tcc_linux.exe
+
+echo "build tcc_boot3.o"
+
+./tcc_linux.exe -nostdinc -nostdlib -c tcc_src/tcc.c -DCONFIG_TRIPLET=\"i386-linux-gnu\" -DTCC_TARGET_I386 -DONE_SOURCE=1 -Wall -O0 -I tcc_src/ ${INCS} -o out.o
+
+# the following shouldn't be necessary, but can be useful for testing
+#rm tcc_linux.exe
+#gcc out.o -o tcc_linux.exe -lm -ldl
+
+mv out.o ../libc_portable_proto/tcc_bin/tcc_boot3.o
+
+echo "building my_libc.o"
+./tcc_linux.exe -nostdinc -nostdlib -c ../libc_portable_proto/my_libc.c ${INCS} -O0 -o out.o
+mv out.o ../libc_portable_proto/my_libc.o
+
+echo "building stubs.o"
+./tcc_linux.exe -nostdlib -nostdinc -c ../libc_portable_proto/stubs.c -O0 -o out.o
+mv out.o ../libc_portable_proto/stubs.o
+
+echo "running second stage boostrap"
+cd ../libc_portable_proto
+
+./bootstrap_tcc.sh
