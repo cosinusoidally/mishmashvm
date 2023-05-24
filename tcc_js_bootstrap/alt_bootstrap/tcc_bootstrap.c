4910fa6090981fd7f4403629c382deea110b07aa
Liam Wilson <cosinusoidally@gmail.com>
2023-05-24 17:18:10 +0000

    more

--- tcc_js_bootstrap/alt_bootstrap/tcc_bootstrap.c
+++ tcc_js_bootstrap/alt_bootstrap/tcc_bootstrap.c
@@ -162,43 +162,11 @@ void *tcc_get_symbol(TCCState *s, const char *name);
 // ELF stuff
 
 typedef uint16_t Elf32_Half;
-typedef uint16_t Elf64_Half;
-
-
 typedef uint32_t Elf32_Word;
-typedef	int32_t  Elf32_Sword;
-typedef uint32_t Elf64_Word;
-typedef	int32_t  Elf64_Sword;
-
-
-typedef uint64_t Elf32_Xword;
-typedef	int64_t  Elf32_Sxword;
-typedef uint64_t Elf64_Xword;
-typedef	int64_t  Elf64_Sxword;
-
-
 typedef uint32_t Elf32_Addr;
-typedef uint64_t Elf64_Addr;
-
-
 typedef uint32_t Elf32_Off;
-typedef uint64_t Elf64_Off;
-
-
 typedef uint16_t Elf32_Section;
-typedef uint16_t Elf64_Section;
-
-
-typedef Elf32_Half Elf32_Versym;
-typedef Elf64_Half Elf64_Versym;
-
-
-
-
-
-
-typedef struct
-{
+typedef struct {
   unsigned char	e_ident[(16)];
   Elf32_Half	e_type;
   Elf32_Half	e_machine;
@@ -214,27 +182,7 @@ typedef struct
   Elf32_Half	e_shnum;
   Elf32_Half	e_shstrndx;
 } Elf32_Ehdr;
-
-typedef struct
-{
-  unsigned char	e_ident[(16)];
-  Elf64_Half	e_type;
-  Elf64_Half	e_machine;
-  Elf64_Word	e_version;
-  Elf64_Addr	e_entry;
-  Elf64_Off	e_phoff;
-  Elf64_Off	e_shoff;
-  Elf64_Word	e_flags;
-  Elf64_Half	e_ehsize;
-  Elf64_Half	e_phentsize;
-  Elf64_Half	e_phnum;
-  Elf64_Half	e_shentsize;
-  Elf64_Half	e_shnum;
-  Elf64_Half	e_shstrndx;
-} Elf64_Ehdr;
-# 282 "tcc_src/elf.h"
-typedef struct
-{
+typedef struct {
   Elf32_Word	sh_name;
   Elf32_Word	sh_type;
   Elf32_Word	sh_flags;
@@ -246,23 +194,7 @@ typedef struct
   Elf32_Word	sh_addralign;
   Elf32_Word	sh_entsize;
 } Elf32_Shdr;
-
-typedef struct
-{
-  Elf64_Word	sh_name;
-  Elf64_Word	sh_type;
-  Elf64_Xword	sh_flags;
-  Elf64_Addr	sh_addr;
-  Elf64_Off	sh_offset;
-  Elf64_Xword	sh_size;
-  Elf64_Word	sh_link;
-  Elf64_Word	sh_info;
-  Elf64_Xword	sh_addralign;
-  Elf64_Xword	sh_entsize;
-} Elf64_Shdr;
-# 392 "tcc_src/elf.h"
-typedef struct
-{
+typedef struct {
   Elf32_Word	st_name;
   Elf32_Addr	st_value;
   Elf32_Word	st_size;
@@ -270,67 +202,11 @@ typedef struct
   unsigned char	st_other;
   Elf32_Section	st_shndx;
 } Elf32_Sym;
-
-typedef struct
-{
-  Elf64_Word	st_name;
-  unsigned char	st_info;
-  unsigned char st_other;
-  Elf64_Section	st_shndx;
-  Elf64_Addr	st_value;
-  Elf64_Xword	st_size;
-} Elf64_Sym;
-
-
-
-
-typedef struct
-{
-  Elf32_Half si_boundto;
-  Elf32_Half si_flags;
-} Elf32_Syminfo;
-
-typedef struct
-{
-  Elf64_Half si_boundto;
-  Elf64_Half si_flags;
-} Elf64_Syminfo;
-# 507 "tcc_src/elf.h"
-typedef struct
-{
+typedef struct {
   Elf32_Addr	r_offset;
   Elf32_Word	r_info;
 } Elf32_Rel;
-
-
-
-
-
-
-typedef struct
-{
-  Elf64_Addr	r_offset;
-  Elf64_Xword	r_info;
-} Elf64_Rel;
-
-
-
-typedef struct
-{
-  Elf32_Addr	r_offset;
-  Elf32_Word	r_info;
-  Elf32_Sword	r_addend;
-} Elf32_Rela;
-
-typedef struct
-{
-  Elf64_Addr	r_offset;
-  Elf64_Xword	r_info;
-  Elf64_Sxword	r_addend;
-} Elf64_Rela;
-# 552 "tcc_src/elf.h"
-typedef struct
-{
+typedef struct {
   Elf32_Word	p_type;
   Elf32_Off	p_offset;
   Elf32_Addr	p_vaddr;
@@ -340,29 +216,6 @@ typedef struct
   Elf32_Word	p_flags;
   Elf32_Word	p_align;
 } Elf32_Phdr;
-
-typedef struct
-{
-  Elf64_Word	p_type;
-  Elf64_Word	p_flags;
-  Elf64_Off	p_offset;
-  Elf64_Addr	p_vaddr;
-  Elf64_Addr	p_paddr;
-  Elf64_Xword	p_filesz;
-  Elf64_Xword	p_memsz;
-  Elf64_Xword	p_align;
-} Elf64_Phdr;
-# 658 "tcc_src/elf.h"
-typedef struct
-{
-  Elf32_Sword	d_tag;
-  union
-    {
-      Elf32_Word d_val;
-      Elf32_Addr d_ptr;
-    } d_un;
-} Elf32_Dyn;
-
 enum {
     TREG_EAX = 0,
     TREG_ECX,
