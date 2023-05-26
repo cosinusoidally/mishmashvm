dda0e0b6c0bf40f112e1c43e8dbe2747ae9f46b3
Liam Wilson <cosinusoidally@gmail.com>
2023-05-26 12:50:30 +0000

    more

--- tcc_js_bootstrap/alt_bootstrap/tcc_bootstrap.c
+++ tcc_js_bootstrap/alt_bootstrap/tcc_bootstrap.c
@@ -3297,13 +3297,16 @@ maybe_newline:
         }
         break;
     case '!':
-{ p++; c = *p; if (c == '\\') { c = handle_stray1(p); p = file->buf_ptr; }}; if (c == '=') { p++; tok = 0x95; } else { tok = '!'; } break;
-//    p=PARSE2(&c,&p,'!', '!', '=', TOK_NE); break;
-
-    case '=': { p++; c = *p; if (c == '\\') { c = handle_stray1(p); p = file->buf_ptr; }}; if (c == '=') { p++; tok = 0x94; } else { tok = '='; } break;
-    case '*': { p++; c = *p; if (c == '\\') { c = handle_stray1(p); p = file->buf_ptr; }}; if (c == '=') { p++; tok = 0xaa; } else { tok = '*'; } break;
-    case '%': { p++; c = *p; if (c == '\\') { c = handle_stray1(p); p = file->buf_ptr; }}; if (c == '=') { p++; tok = 0xa5; } else { tok = '%'; } break;
-    case '^': { p++; c = *p; if (c == '\\') { c = handle_stray1(p); p = file->buf_ptr; }}; if (c == '=') { p++; tok = 0xde; } else { tok = '^'; } break;
+//{ p++; c = *p; if (c == '\\') { c = handle_stray1(p); p = file->buf_ptr; }}; if (c == '=') { p++; tok = 0x95; } else { tok = '!'; } break;
+    p=PARSE2(&c,&p, '!', '=', TOK_NE); break;
+    case '=':
+{ p++; c = *p; if (c == '\\') { c = handle_stray1(p); p = file->buf_ptr; }}; if (c == '=') { p++; tok = 0x94; } else { tok = '='; } break;
+    case '*':
+{ p++; c = *p; if (c == '\\') { c = handle_stray1(p); p = file->buf_ptr; }}; if (c == '=') { p++; tok = 0xaa; } else { tok = '*'; } break;
+    case '%':
+{ p++; c = *p; if (c == '\\') { c = handle_stray1(p); p = file->buf_ptr; }}; if (c == '=') { p++; tok = 0xa5; } else { tok = '%'; } break;
+    case '^':
+{ p++; c = *p; if (c == '\\') { c = handle_stray1(p); p = file->buf_ptr; }}; if (c == '=') { p++; tok = 0xde; } else { tok = '^'; } break;
     case '/':
         p=PEEKC(&c,&p);
         if (c == '*') {
