5db0b2c943b9949834379db6c71568a05dcce088
Liam Wilson <cosinusoidally@gmail.com>
2023-05-30 16:45:59 +0000

    add in libtcc1_bootstrap_min.c

---
+++ tcc_js_bootstrap/alt_bootstrap/libtcc1_bootstrap_min.c
@@ -0,0 +1,420 @@
+/* TCC runtime library. 
+   Parts of this code are (c) 2002 Fabrice Bellard 
+
+   Copyright (C) 1987, 1988, 1992, 1994, 1995 Free Software Foundation, Inc.
+
+This file is free software; you can redistribute it and/or modify it
+under the terms of the GNU General Public License as published by the
+Free Software Foundation; either version 2, or (at your option) any
+later version.
+
+In addition to the permissions in the GNU General Public License, the
+Free Software Foundation gives you unlimited permission to link the
+compiled version of this file into combinations with other programs,
+and to distribute those combinations without any restriction coming
+from the use of this file.  (The General Public License restrictions
+do apply in other respects; for example, they cover modification of
+the file, and distribution when not linked into a combine
+executable.)
+
+This file is distributed in the hope that it will be useful, but
+WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program; see the file COPYING.  If not, write to
+the Free Software Foundation, 59 Temple Place - Suite 330,
+Boston, MA 02111-1307, USA.  
+*/
+
+/*
+
+This also contains some cobbled together code from:
+
+udivmoddi4.c
+ashldi3.c
+
+from the LLVM compiler-rt project
+
+*/
+
+#define W_TYPE_SIZE   32
+#define BITS_PER_UNIT 8
+
+typedef int Wtype;
+typedef unsigned int UWtype;
+typedef unsigned int USItype;
+typedef long long DWtype;
+typedef unsigned long long UDWtype;
+
+struct DWstruct {
+    Wtype low, high;
+};
+
+typedef union
+{
+  struct DWstruct s;
+  DWtype ll;
+} DWunion;
+
+typedef long double XFtype;
+#define WORD_SIZE (sizeof (Wtype) * BITS_PER_UNIT)
+#define HIGH_WORD_COEFF (((UDWtype) 1) << WORD_SIZE)
+
+/* the following deal with IEEE single-precision numbers */
+#define EXCESS		126
+#define SIGNBIT		0x80000000
+#define HIDDEN		(1 << 23)
+#define SIGN(fp)	((fp) & SIGNBIT)
+#define EXP(fp)		(((fp) >> 23) & 0xFF)
+#define MANT(fp)	(((fp) & 0x7FFFFF) | HIDDEN)
+#define PACK(s,e,m)	((s) | ((e) << 23) | (m))
+
+/* the following deal with IEEE double-precision numbers */
+#define EXCESSD		1022
+#define HIDDEND		(1 << 20)
+#define EXPD(fp)	(((fp.l.upper) >> 20) & 0x7FF)
+#define SIGND(fp)	((fp.l.upper) & SIGNBIT)
+#define MANTD(fp)	(((((fp.l.upper) & 0xFFFFF) | HIDDEND) << 10) | \
+				(fp.l.lower >> 22))
+#define HIDDEND_LL	((long long)1 << 52)
+#define MANTD_LL(fp)	((fp.ll & (HIDDEND_LL-1)) | HIDDEND_LL)
+#define PACKD_LL(s,e,m)	(((long long)((s)+((e)<<20))<<32)|(m))
+
+/* the following deal with x86 long double-precision numbers */
+#define EXCESSLD	16382
+#define EXPLD(fp)	(fp.l.upper & 0x7fff)
+#define SIGNLD(fp)	((fp.l.upper) & 0x8000)
+
+typedef long long di_int;
+typedef int si_int;
+typedef unsigned su_int;
+#define CHAR_BIT 8;
+typedef unsigned long long du_int;
+typedef union
+{
+    du_int all;
+    struct
+    {
+        su_int low;
+        su_int high;
+    }s;
+} udwords;
+
+typedef union
+{
+    di_int all;
+    struct
+    {
+        su_int low;
+        si_int high;
+    }s;
+} dwords;
+
+static du_int __udivmoddi4(du_int a, du_int b, du_int* rem);
+
+unsigned long long __udivdi3(unsigned long long u, unsigned long long v)
+{
+puts("called __udivdi3\n");
+    return __udivmoddi4 (u, v, (UDWtype *) 0);
+}
+
+unsigned long long __umoddi3(unsigned long long u, unsigned long long v)
+{
+puts("called __umoddi3\n");
+    UDWtype w;
+    __udivmoddi4 (u, v, &w);
+    return w;
+}
+
+unsigned long long __lshrdi3(unsigned long long a, int b)
+{
+    DWunion u;
+    u.ll = a;
+    if (b >= 32) {
+        u.s.low = (unsigned)u.s.high >> (b - 32);
+        u.s.high = 0;
+    } else if (b != 0) {
+        u.s.low = ((unsigned)u.s.low >> b) | (u.s.high << (32 - b));
+        u.s.high = (unsigned)u.s.high >> b;
+    }
+    return u.ll;
+}
+
+long long __ashldi3(long long a, int b)
+{
+    DWunion u;
+    u.ll = a;
+    if (b >= 32) {
+        u.s.high = (unsigned)u.s.low << (b - 32);
+        u.s.low = 0;
+    } else if (b != 0) {
+        u.s.high = ((unsigned)u.s.high << b) | ((unsigned)u.s.low >> (32 - b));
+        u.s.low = (unsigned)u.s.low << b;
+    }
+    return u.ll;
+}
+
+// dummy symbols
+__floatundidf(){ }
+__floatundixf() { }
+__fixunsxfdi(){ }
+
+static di_int __ashrdi3(di_int a, int b) {
+  int bits_in_word = sizeof(si_int) * CHAR_BIT;
+  dwords input;
+  dwords result;
+  input.all = a;
+  if (b & bits_in_word) /* bits_in_word <= b < bits_in_dword */ {
+    // result.s.high = input.s.high < 0 ? -1 : 0
+    result.s.high = input.s.high >> (bits_in_word - 1);
+    result.s.low = input.s.high >> (b - bits_in_word);
+  } else /* 0 <= b < bits_in_word */ {
+    if (b == 0)
+      return a;
+    result.s.high = input.s.high >> b;
+    result.s.low = (input.s.high << (bits_in_word - b)) | (input.s.low >> b);
+  }
+  return result.all;
+}
+
+__builtin_ctz(){
+    puts("__builtin_ctz\n");
+    exit(1);
+}
+
+__builtin_clz(){
+    puts("__builtin_clz\n");
+    exit(1);
+}
+
+static du_int __udivmoddi4(du_int a, du_int b, du_int* rem) {
+    puts("called __udivmoddi4\n");
+    const unsigned n_uword_bits = sizeof(su_int) * CHAR_BIT;
+    const unsigned n_udword_bits = sizeof(du_int) * CHAR_BIT;
+    udwords n;
+    n.all = a;
+    udwords d;
+    d.all = b;
+    udwords q;
+    udwords r;
+    unsigned sr;
+    /* special cases, X is unknown, K != 0 */
+    if (n.s.high == 0)
+    {
+        if (d.s.high == 0)
+        {
+            /* 0 X
+             * ---
+             * 0 X
+             */
+            if (rem)
+                *rem = n.s.low % d.s.low;
+            return n.s.low / d.s.low;
+        }
+        /* 0 X
+         * ---
+         * K X
+         */
+        if (rem)
+            *rem = n.s.low;
+        return 0;
+    }
+    /* n.s.high != 0 */
+    if (d.s.low == 0)
+    {
+        if (d.s.high == 0)
+        {
+            /* K X
+             * ---
+             * 0 0
+             */ 
+            if (rem)
+                *rem = n.s.high % d.s.low;
+            return n.s.high / d.s.low;
+        }
+        /* d.s.high != 0 */
+        if (n.s.low == 0)
+        {
+            /* K 0
+             * ---
+             * K 0
+             */
+            if (rem)
+            {
+                r.s.high = n.s.high % d.s.high;
+                r.s.low = 0;
+                *rem = r.all;
+            }
+            return n.s.high / d.s.high;
+        }
+        /* K K
+         * ---
+         * K 0
+         */
+        if ((d.s.high & (d.s.high - 1)) == 0)     /* if d is a power of 2 */
+        {
+            if (rem)
+            {
+                r.s.low = n.s.low;
+                r.s.high = n.s.high & (d.s.high - 1);
+                *rem = r.all;
+            }
+            return n.s.high >> __builtin_ctz(d.s.high);
+        }
+        /* K K
+         * ---
+         * K 0
+         */
+        sr = __builtin_clz(d.s.high) - __builtin_clz(n.s.high);
+        /* 0 <= sr <= n_uword_bits - 2 or sr large */
+        if (sr > n_uword_bits - 2)
+        {
+           if (rem)
+                *rem = n.all;
+            return 0;
+        }
+        ++sr;
+        /* 1 <= sr <= n_uword_bits - 1 */
+        /* q.all = n.all << (n_udword_bits - sr); */
+        q.s.low = 0;
+        q.s.high = n.s.low << (n_uword_bits - sr);
+        /* r.all = n.all >> sr; */
+        r.s.high = n.s.high >> sr;
+        r.s.low = (n.s.high << (n_uword_bits - sr)) | (n.s.low >> sr);
+    }
+    else  /* d.s.low != 0 */
+    {
+        if (d.s.high == 0)
+        {
+            /* K X
+             * ---
+             * 0 K
+             */
+            if ((d.s.low & (d.s.low - 1)) == 0)     /* if d is a power of 2 */
+            {
+                if (rem)
+                    *rem = n.s.low & (d.s.low - 1);
+                if (d.s.low == 1)
+                    return n.all;
+                sr = __builtin_ctz(d.s.low);
+                q.s.high = n.s.high >> sr;
+                q.s.low = (n.s.high << (n_uword_bits - sr)) | (n.s.low >> sr);
+                return q.all;
+            }
+            /* K X
+             * ---
+             *0 K
+             */
+            sr = 1 + n_uword_bits + __builtin_clz(d.s.low) - __builtin_clz(n.s.high);
+            /* 2 <= sr <= n_udword_bits - 1
+             * q.all = n.all << (n_udword_bits - sr);
+             * r.all = n.all >> sr;
+             * if (sr == n_uword_bits)
+             * {
+             *     q.s.low = 0;
+             *     q.s.high = n.s.low;
+             *     r.s.high = 0;
+             *     r.s.low = n.s.high;
+             * }
+             * else if (sr < n_uword_bits)  // 2 <= sr <= n_uword_bits - 1
+             * {
+             *     q.s.low = 0;
+             *     q.s.high = n.s.low << (n_uword_bits - sr);
+             *     r.s.high = n.s.high >> sr;
+             *     r.s.low = (n.s.high << (n_uword_bits - sr)) | (n.s.low >> sr);
+             * }
+             * else              // n_uword_bits + 1 <= sr <= n_udword_bits - 1
+             * {
+             *     q.s.low = n.s.low << (n_udword_bits - sr);
+             *     q.s.high = (n.s.high << (n_udword_bits - sr)) |
+             *              (n.s.low >> (sr - n_uword_bits));
+             *     r.s.high = 0;
+             *     r.s.low = n.s.high >> (sr - n_uword_bits);
+             * }
+             */
+            q.s.low =  (n.s.low << (n_udword_bits - sr)) &
+                     ((si_int)(n_uword_bits - sr) >> (n_uword_bits-1));
+            q.s.high = ((n.s.low << ( n_uword_bits - sr))                       &
+                     ((si_int)(sr - n_uword_bits - 1) >> (n_uword_bits-1))) |
+                     (((n.s.high << (n_udword_bits - sr))                     |
+                     (n.s.low >> (sr - n_uword_bits)))                        &
+                     ((si_int)(n_uword_bits - sr) >> (n_uword_bits-1)));
+            r.s.high = (n.s.high >> sr) &
+                     ((si_int)(sr - n_uword_bits) >> (n_uword_bits-1));
+            r.s.low =  ((n.s.high >> (sr - n_uword_bits))                       &
+                     ((si_int)(n_uword_bits - sr - 1) >> (n_uword_bits-1))) |
+                     (((n.s.high << (n_uword_bits - sr))                      |
+                     (n.s.low >> sr))                                         &
+                     ((si_int)(sr - n_uword_bits) >> (n_uword_bits-1)));
+        }
+        else
+        {
+            /* K X
+             * ---
+             * K K
+             */
+            sr = __builtin_clz(d.s.high) - __builtin_clz(n.s.high);
+            /* 0 <= sr <= n_uword_bits - 1 or sr large */
+            if (sr > n_uword_bits - 1)
+            {
+               if (rem)
+                    *rem = n.all;
+                return 0;
+            }
+            ++sr;
+            /* 1 <= sr <= n_uword_bits */
+            /*  q.all = n.all << (n_udword_bits - sr); */
+            q.s.low = 0;
+            q.s.high = n.s.low << (n_uword_bits - sr);
+            /* r.all = n.all >> sr;
+             * if (sr < n_uword_bits)
+             * {
+             *     r.s.high = n.s.high >> sr;
+             *     r.s.low = (n.s.high << (n_uword_bits - sr)) | (n.s.low >> sr);
+             * }
+             * else
+             * {
+             *     r.s.high = 0;
+             *     r.s.low = n.s.high;
+             * }
+             */
+            r.s.high = (n.s.high >> sr) &
+                     ((si_int)(sr - n_uword_bits) >> (n_uword_bits-1));
+            r.s.low = (n.s.high << (n_uword_bits - sr)) |
+                    ((n.s.low >> sr)                  &
+                    ((si_int)(sr - n_uword_bits) >> (n_uword_bits-1)));
+        }
+    }
+    /* Not a special case
+     * q and r are initialized with:
+     * q.all = n.all << (n_udword_bits - sr);
+     * r.all = n.all >> sr;
+     * 1 <= sr <= n_udword_bits - 1
+     */
+    su_int carry = 0;
+    for (; sr > 0; --sr)
+    {
+        /* r:q = ((r:q)  << 1) | carry */
+        r.s.high = (r.s.high << 1) | (r.s.low  >> (n_uword_bits - 1));
+        r.s.low  = (r.s.low  << 1) | (q.s.high >> (n_uword_bits - 1));
+        q.s.high = (q.s.high << 1) | (q.s.low  >> (n_uword_bits - 1));
+        q.s.low  = (q.s.low  << 1) | carry;
+        /* carry = 0;
+         * if (r.all >= d.all)
+         * {
+         *      r.all -= d.all;
+         *      carry = 1;
+         * }
+         */
+        di_int s;
+        s = (di_int)(d.all - r.all - 1) >> (n_udword_bits - 1);
+        carry = s & 1;
+        r.all -= d.all & s;
+    }
+    q.all = (q.all << 1) | carry;
+    if (rem)
+        *rem = r.all;
+    return q.all;
+}
