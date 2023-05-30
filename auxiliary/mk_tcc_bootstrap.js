14cfa1ddf23209e200d688b105c06c8c4a1be158
Liam Wilson <cosinusoidally@gmail.com>
2023-05-30 00:52:37 +0000

    add alt tcc js based boostrap

---
+++ auxiliary/mk_tcc_bootstrap.js
@@ -0,0 +1,7 @@
+args="-nostdlib -nostdinc -c tcc_bootstrap.c -o out.o";
+args=args.split(" ");
+print(JSON.stringify(args));
+load("../tcc_js_bootstrap/tcc_em.js");
+FS.writeFile("/tcc_bootstrap.c", read("../tcc_js_bootstrap/alt_bootstrap/tcc_bootstrap.c"));
+Module.arguments=args;
+compile("../tcc_js_bootstrap/out.o");
