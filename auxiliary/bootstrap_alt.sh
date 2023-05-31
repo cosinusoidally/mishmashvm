5a92e631329e0741764543a5fddda9329f518258
Liam Wilson <cosinusoidally@gmail.com>
2023-05-31 00:38:17 +0000

    more

--- auxiliary/bootstrap_alt.sh
+++ auxiliary/bootstrap_alt.sh
@@ -1,4 +1,4 @@
-INCS="-I ../includes/tmp/tcc/lib/tcc/include/ -I ../includes/usr/include/i386-linux-gnu/  -I ../includes/usr/include/"
+#INCS="-I ../includes/tmp/tcc/lib/tcc/include/ -I ../includes/usr/include/i386-linux-gnu/  -I ../includes/usr/include/"
 
 ./bootstrap_cleanup_tcc.sh
 
@@ -32,9 +32,9 @@ echo "build tcc_boot3.o"
 
 PHASE1_ARGS="-nostdinc -nostdlib -c ./alt_bootstrap/tcc_bootstrap.c -o out.o"
 
-PHASE2_1_ARGS="-DBOOTSTRAP_MODE=1 -nostdinc -nostdlib -c tcc_src/tcc.c -DCONFIG_TRIPLET=\"i386-linux-gnu\" -DTCC_TARGET_I386 -DONE_SOURCE=1 -I tcc_src/ ${INCS} -o out.o"
+PHASE2_1_ARGS="-DBOOTSTRAP_MODE=1 -nostdinc -nostdlib -c ../tcc_js_bootstrap/tcc_src/tcc.c -DCONFIG_TRIPLET=\"i386-linux-gnu\" -DTCC_TARGET_I386 -DONE_SOURCE=1 -I ../tcc_js_bootstrap/tcc_src/ -I ../includes/tmp/tcc/lib/tcc/include/ -I ../includes/usr/include/i386-linux-gnu/  -I ../includes/usr/include/ -o tcc_bin/tcc_boot3.o"
 
-PHASE2_2_ARGS="-nostdinc -nostdlib -c ../tcc_src/tcc.c -DCONFIG_TRIPLET=\"i386-linux-gnu\" -DTCC_TARGET_I386 -DONE_SOURCE=1 -I ../tcc_src/ ${INCS} -o out.o"
+#PHASE2_2_ARGS="-nostdinc -nostdlib -c ../tcc_src/tcc.c -DCONFIG_TRIPLET=\"i386-linux-gnu\" -DTCC_TARGET_I386 -DONE_SOURCE=1 -I ../tcc_src/ ${INCS} -o out.o"
 
 rm libtcc1.o
 ./tcc_linux.exe ${LIBTCC1_ARGS}
@@ -62,7 +62,9 @@ rm out.o
 
 # phase 2
 
-./tcc_linux.exe ${PHASE2_1_ARGS}
+cd ../libc_portable_proto/
+../tcc_js_bootstrap/tcc_linux.exe ${PHASE2_1_ARGS}
+cd ../tcc_js_bootstrap
 #rm tcc_linux.exe
 #tcc_bootstrap.exe ${LINK_CMD}
 #chmod +x tcc_linux.exe
@@ -85,7 +87,6 @@ rm out.o
 #tcc_bootstrap.exe ${LINK_CMD}
 #chmod +x tcc_linux.exe
 
-mv out.o ../libc_portable_proto/tcc_bin/tcc_boot3.o
 mv libtcc1.o ../libc_portable_proto/tcc_bin/libtcc1.o
 
 cd ../libc_portable_proto
