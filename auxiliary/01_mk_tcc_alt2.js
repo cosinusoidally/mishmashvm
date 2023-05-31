8fa60cf512317749b68733b4288e352629038694
Liam Wilson <cosinusoidally@gmail.com>
2023-05-31 17:29:28 +0000

    start stubbing out a js based C compiler

--- auxiliary/01_mk_tcc_alt2.js
+++ auxiliary/01_mk_tcc_alt2.js
@@ -2,6 +2,7 @@ args="-nostdlib -nostdinc -c tcc_bootstrap.c -o out.o";
 args=args.split(" ");
 print(JSON.stringify(args));
 load("../../tcc_js_port/a.out.js");
+load("js_cc_proto.js");
 FS.writeFile("/tcc_bootstrap.c", read("../tcc_js_bootstrap/alt_bootstrap/tcc_bootstrap.c"));
 Module.arguments=args;
 compile("../libc_portable_proto/tcc_bin/tcc_boot3.o");
