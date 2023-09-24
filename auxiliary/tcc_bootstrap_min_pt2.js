9647415a3e09142810e28260a1847751ce918685
Liam Wilson <cosinusoidally@gmail.com>
2023-09-24 19:57:54 +0000

    refactor

--- auxiliary/tcc_bootstrap_min_pt2.js
+++ auxiliary/tcc_bootstrap_min_pt2.js
@@ -6,68 +6,6 @@ libc.chdir("../../tcc_bootstrap_alt/tcc_10/");
 
 tcc_10_o=mm.decode_elf(read("tcc.o","binary"));
 
-
-passthrough={
-  "puts": true,
-  "realloc": true,
-  "malloc": true,
-  "memcpy": true,
-  "strlen": true,
-  "memset": true,
-  "printf": true,
-  "fopen": true,
-  "getc_unlocked": true,
-  "memcpy": true,
-  "memcmp": true,
-  "strcpy": true,
-  "strcat": true,
-  "strdup": true,
-  "fclose": true,
-  "free": true,
-  "memmove": true,
-  "strrchr": true,
-  "strcmp": true,
-  "fprintf": true,
-  "vfprintf": true,
-  "exit": true,
-  "strtod": true,
-  "strtof": true,
-  "strchr": true,
-  "atoi": true,
-  "open": true,
-  "read": true,
-  "close": true,
-  "sprintf": true,
-  "snprintf": true,
-  "fdopen": true,
-  "fwrite": true,
-  "fputc": true,
-  "ldexp": true,
-// FIXME remove use of setjmp
-  "_setjmp": true,
-  "strtoul": true,
-  "sscanf": true,
-  "vsnprintf": true,
-// FIXME get rid of this
-  "unlink": true,
-  "stdout": true,
-  "stderr": true,
-};
-
-my_wrap={};
-exports=[];
-my_wrap.exports=exports;
-my_wrap.relocate_all=function(){};
-
-for(i in passthrough){
- a=ctypes.cast(
-      libc.lib.declare(i,ctypes.default_abi,ctypes.uint32_t),
-      ctypes.uint32_t
-      ).value;
-  print(i+" "+a);
-  exports.push({st_name:i,address:a});
-}
-
 tcc_10=mm.link([tcc_10_o,my_wrap]);
 
 main=mm.arg_wrap(tcc_10.get_fn("main"));
