d86fbcc8d2d4a3d525802e9d70280f3787d9d963
Liam Wilson <cosinusoidally@gmail.com>
2023-09-24 15:55:08 +0000

    hacking up code so can load without bootstrap

--- auxiliary/tcc_bootstrap_min.js
+++ auxiliary/tcc_bootstrap_min.js
@@ -1,3 +1,11 @@
-load("lib/setup_platform.js");
-load("lib/mishmashvm_lib.js");
-load("lib/elf_loader.js");
+load("../lib/setup_platform.js");
+quit_orig=quit;
+quit=function(){};
+try {
+  load("../lib/mishmashvm_lib.js");
+} catch (e){
+  //swallow errror
+  print("swallowing error");
+}
+quit=quit_orig;
+load("../lib/elf_loader.js");
