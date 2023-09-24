e1cad628b336c86b8184e9418b80a8de4a60407e
Liam Wilson <cosinusoidally@gmail.com>
2023-09-24 18:13:52 +0000

    more

--- auxiliary/tcc_bootstrap_min.js
+++ auxiliary/tcc_bootstrap_min.js
@@ -119,21 +119,29 @@ while(global_relocs_table<m){
   l=strlen(global_relocs_table);
   var s=mk_js_string(global_relocs_table);
   global_relocs_table+=l+1;
-  print("global_reloc: "+to_hex(global_relocs_table)+" "+l+" "+s);
+  var a=ctypes.cast(
+      libc.lib.declare(s,ctypes.default_abi,ctypes.uint32_t),
+      ctypes.uint32_t
+      ).value;
+  print("global_reloc: "+to_hex(global_relocs_table)+" "+l+" "+s+" "+to_hex(a));
+
   n=ri32(global_relocs_table);
   global_relocs_table+=4;
   print("global_reloc_num: "+n);
   for(i=0;i<n;i++){
 
     off=ri32(global_relocs_base+goff+4);
+    addr=off+prog_rel;
     switch(ri32(global_relocs_base+goff)) {
         case RELOC_ADDR32:
           print("Reloc type RELOC_ADDR32 at "+to_hex(off));
 //          *(int *)addr=a;
+          wi32(addr,a);
           break;
         case RELOC_REL32:
           print("Reloc type RELOC_REL32 at "+to_hex(off));
 //          *(int *)addr = a - addr - 4;
+          wi32(addr,a - (addr+prog-prog_rel) - 4);
           break;
         default:
           err();
@@ -141,3 +149,18 @@ while(global_relocs_table<m){
     goff=goff+8;
   }
 }
+
+p2=new Uint8Array(text_len);
+for(var i=0;i<text_len;i++){
+  p2[i]=ri8(i+prog_rel);
+}
+libc.memcpy(prog,p2,text_len);
+
+real_entrypoint=unsigned(prog+entrypoint);
+print("main: "+to_hex(real_entrypoint));
+
+tcc_o={
+  exports: [{st_name:"main",address:real_entrypoint}],
+  relocate_all:function(){}
+}
+main=mm.link([tcc_o]).get_fn("main");
