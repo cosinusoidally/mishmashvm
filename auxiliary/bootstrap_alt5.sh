dc3ecfeb40bbc025b11e736081bb7bd86c0ee2cc
Liam Wilson <cosinusoidally@gmail.com>
2024-04-06 20:04:31 +0000

    more

--- auxiliary/bootstrap_alt5.sh
+++ auxiliary/bootstrap_alt5.sh
@@ -3,11 +3,9 @@ cd ../../tcc_bootstrap_alt
 pushd .
 cd otccelf/tcc_wrap
 export PATH=$PWD:$PATH
+cd ../artifacts/
+export PATH=$PWD:$PATH
 popd
-
-# make duktape
-
-./mk_duk_otccelf
 
 cd ../mishmashvm/libc_portable_proto
 ./bootstrap_clean.sh
@@ -15,4 +13,17 @@ tcc_wrap -nostdinc -nostdlib -o tcc_bin/tcc_boot3.o -c ../tcc_src/tcc.c -D__linu
 tcc_wrap -nostdinc -nostdlib -o my_libc.o -c my_libc.c -O0 -I ../includes/usr/include/:../includes/usr/include/i386-linux-gnu/:../includes/tmp/tcc/lib/tcc/include/
 tcc_wrap -nostdinc -nostdlib -o stubs.o -c stubs.c -O0 -I ../includes/usr/include/:../includes/usr/include/i386-linux-gnu/:../includes/tmp/tcc/lib/tcc/include/
 
-./bootstrap_tcc.sh
+cd ../auxiliary
+
+# make duktape
+
+./mk_duk_otccelf
+
+cd ../artifacts
+export PATH=$PWD:$PATH
+
+cd ../libc_portable_proto
+js
+pwd
+
+# ./bootstrap_tcc.sh
