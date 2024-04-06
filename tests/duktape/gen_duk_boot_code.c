f1bc8766b027a5552d1391eae6dbd1aec2296417
Liam Wilson <cosinusoidally@gmail.com>
2024-04-06 20:42:26 +0000

    more

--- tests/duktape/gen_duk_boot_code.c
+++ tests/duktape/gen_duk_boot_code.c
@@ -1,9 +1,11 @@
 main(){
-  int in, out;
+  int in, out,c;
   puts("generating duk_boot_code.h");
   in=fopen("tests/duktape/duk_mishmashvm_support.js", "rb");
   out=fopen("artifacts/duk_boot_code2.h", "wb");
-  fputc('A',out);
+  while((c=fgetc(in)) != -1){
+    fputc(c,out);
+  }
   fclose(out);
   exit(0);
 }
