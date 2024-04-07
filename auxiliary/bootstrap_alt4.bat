85bb777be80c362c85c186dfbd467c1390282787
Liam Wilson <cosinusoidally@gmail.com>
2024-04-07 19:07:04 +0000

    enable all steps for bootstrap_alt4.bat

--- auxiliary/bootstrap_alt4.bat
+++ auxiliary/bootstrap_alt4.bat
@@ -1,6 +1,7 @@
 set js_cmd=../../jsshell/js.exe
 set start_cmd=start /wait /B
-REM %start_cmd% %js_cmd% tcc_bootstrap_min_pt0.js
+%start_cmd% %js_cmd% tcc_bootstrap_min_pt0.js
+%start_cmd% %js_cmd% tcc_bootstrap_min_pt1.js
 %start_cmd% %js_cmd% tcc_bootstrap_min_pt2.js
 %start_cmd% %js_cmd% tcc_bootstrap_min_pt3.js
 %start_cmd% %js_cmd% tcc_bootstrap_min_pt4.js
