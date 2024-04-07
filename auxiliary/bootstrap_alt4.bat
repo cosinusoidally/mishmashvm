6fb23d015dc2ae963f51ff6193203439b1aa293d
Liam Wilson <cosinusoidally@gmail.com>
2024-04-07 18:04:38 +0000

    more

--- auxiliary/bootstrap_alt4.bat
+++ auxiliary/bootstrap_alt4.bat
@@ -2,3 +2,6 @@ set js_cmd=../../jsshell/js.exe
 set start_cmd=start /wait /B
 REM %start_cmd% %js_cmd% tcc_bootstrap_min_pt0.js
 %start_cmd% %js_cmd% tcc_bootstrap_min_pt2.js
+%start_cmd% %js_cmd% tcc_bootstrap_min_pt3.js
+%start_cmd% %js_cmd% tcc_bootstrap_min_pt4.js
+%start_cmd% %js_cmd% tcc_bootstrap_min_pt5.js
