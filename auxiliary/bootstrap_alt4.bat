7fd8dd54d6e9246d3741c470cddf0d056c74c334
Liam Wilson <cosinusoidally@gmail.com>
2024-04-07 18:23:49 +0000

    more

--- auxiliary/bootstrap_alt4.bat
+++ auxiliary/bootstrap_alt4.bat
@@ -5,3 +5,12 @@ REM %start_cmd% %js_cmd% tcc_bootstrap_min_pt0.js
 %start_cmd% %js_cmd% tcc_bootstrap_min_pt3.js
 %start_cmd% %js_cmd% tcc_bootstrap_min_pt4.js
 %start_cmd% %js_cmd% tcc_bootstrap_min_pt5.js
+
+cd ..
+del libc_portable_proto\tcc_bin\libtcc1.o
+del libc_portable_proto\tcc_bin\libtcc1.o.new
+del libc_portable_proto\tcc_bin\tcc_boot3.o
+del libc_portable_proto\tcc_bin\tcc_boot3.o.new
+move tcc_js_bootstrap\out.o  libc_portable_proto\tcc_bin\tcc_boot3.o
+cd libc_portable_proto
+bootstrap_tcc.bat
