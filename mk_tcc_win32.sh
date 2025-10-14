f469b7bc2ad79e2a0b1a66fbd24e24126476a307
Liam Wilson <cosinusoidally@gmail.com>
2025-10-14 09:48:24 +0100

    win32 hello.exe building ok

--- mk_tcc_win32.sh
+++ mk_tcc_win32.sh
@@ -1,2 +1,6 @@
 ./mk_clean_artifacts.sh
+mkdir -p artifacts/tcc_win32/lib
+cp -r tcc_src/win32/include artifacts/tcc_win32/include
+cp tcc_src/include/* artifacts/tcc_win32/include
+cp tcc_src/win32/lib/*def artifacts/tcc_win32/lib
 js -e 'load("mishmashvm.js"); test(25)'
