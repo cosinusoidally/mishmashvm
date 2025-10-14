6a453ed97421d59f99ba8321848f02028ced8944
Liam Wilson <cosinusoidally@gmail.com>
2025-10-14 10:48:29 +0100

    more

--- mk_tcc_win32.sh
+++ mk_tcc_win32.sh
@@ -3,5 +3,5 @@ mkdir -p artifacts/tcc_win32/lib
 cp -r tcc_src/win32/include artifacts/tcc_win32/include
 cp tcc_src/include/* artifacts/tcc_win32/include
 cp tcc_src/win32/lib/*def artifacts/tcc_win32/lib
-js -e 'load("mishmashvm.js"); test(25)'
+js -e "load('mishmashvm.js'); test(25)"
 echo "If there were no errors then there should now be a copy of tcc.exe in artifacts/tcc_win32 . You should be able to copy that directory to wherever you want."
