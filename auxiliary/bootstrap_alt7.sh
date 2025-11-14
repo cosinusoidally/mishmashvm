da42ef9c0d827d00e9e9267a939dedc7157ba864
Liam Wilson <cosinusoidally@gmail.com>
2025-11-14 12:29:53 +0000

    more

--- auxiliary/bootstrap_alt7.sh
+++ auxiliary/bootstrap_alt7.sh
@@ -13,4 +13,16 @@ cat mmvm_boot.list >> artifacts/full.list
 
 time ./artifacts/test_tracer.exe ./artifacts/min_linux.exe artifacts/full.list
 
+popd
+
+cd ../libc_portable_proto/
+
+./bootstrap_clean.sh
+
+cp ../../tcc_simple/experiments/xp_linux/artifacts/stubs.o .
+cp ../../tcc_simple/experiments/xp_linux/artifacts/my_libc.o .
+cp ../../tcc_simple/experiments/xp_linux/artifacts/tcc_27_4.o tcc_bin/tcc_boot3.o
+
+./bootstrap_tcc.sh
+
 echo "DONE"
